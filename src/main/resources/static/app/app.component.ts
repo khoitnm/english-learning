beff66405decc4ebe4d92ce8f10e9306626de928
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2017-01-25 16:55:57 +0700

    Use TypeScript from AngularJS tutorial

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,31 +1,25 @@
-import { Component } from '@angular/core';
-
-export class Hero {
-    id:number;
-    name:string;
-}
+import { Component }          from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    `
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
-    hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+  title = 'Tour of Heroes';
 }
+
+
 /*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
